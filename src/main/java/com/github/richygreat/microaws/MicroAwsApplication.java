744ae92ad2aa26eddb59b90fc0a22927fc944909
Richy Great <richygreat@gmail.com>
2019-02-10 10:17:33 +0100

    Initial Commit

---
+++ src/main/java/com/github/richygreat/microaws/MicroAwsApplication.java
@@ -0,0 +1,11 @@
+package com.github.richygreat.microaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAwsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAwsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/richygreat/microaws/MicroAwsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.github.richygreat.microaws;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MicroAwsApplicationTest {
+	@Test
+	void testSuccess() {
+		assertTrue(true);
+	}
+}
